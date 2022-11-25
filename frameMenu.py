6876273f7e8341d7ddbabf4a0226a98aaebb648d
Andei Smirnov <sm1hero4ka@ya.ru>
2022-11-25 17:29:11 +0300

    Add btns

--- frameMenu.py
+++ frameMenu.py
@@ -8,14 +8,18 @@ class interfaceMenu():
               font=(c.TEXT_FONT, c.TEXT_FONT_SIZE),
                 width=c.BUTTON_WIDTH, height=c.BUTTON_HEIGHT)
         
-        btnVideo = tk.Button(window, text="О нас", font=(c.TEXT_FONT, c.TEXT_FONT_SIZE))
+        btnVideo  = tk.Button(window, text="Видео о нас",
+              font=(c.TEXT_FONT, c.TEXT_FONT_SIZE),
+                width=c.BUTTON_WIDTH, height=c.BUTTON_HEIGHT)
         
         btnRasp.grid( column=0, row=0)
         btnVideo.grid(column=1, row=0)
 
-        btnRasp.place(x = ((int(c.WINDOW_WIDTH) / 2) - int(c.BUTTON_WIDTH * 20)),
-                      y = ((int(c.WINDOW_HEIGHT) / 2) + int(c.BUTTON_HEIGHT * 100)))
+        btnRasp.place(x = ((int(c.WINDOW_WIDTH) / 2)) - 200)
         
+        btnVideo.place()
+        
+
         return window
 
     
